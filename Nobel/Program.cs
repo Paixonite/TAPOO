ab54d9e7046f03292d52c27753dd2bef68ffb7a2
Vítor Paixão <paixaodamasceno@gmail.com>
2025-06-11 16:23:52 -0300

    finished senha

---
+++ Nobel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using System.IO;
+
+var filePath = "prize.json";
+var str = File.ReadAllText(filePath);
+var regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_\-{}\[\]:;""'?<>,.]).{7,16}$");
+
+
+
+// Imprime matches
+foreach (Match match in regex.Matches(str))
+{
+    foreach (Group group in match.Groups)
+    {
+        if (group.Success)
+        {
+            Console.WriteLine(group.Value);
+        }
+    }
+}
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,1 +1,27 @@
 ﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Regras:");
+Console.WriteLine("A senha deve ter entre 7 e 16 caracteres.");
+Console.WriteLine("Deve conter pelo menos uma letra minúscula (a-z).");
+Console.WriteLine("Deve conter pelo menos uma letra maiúscula (A-Z).");
+Console.WriteLine("Deve conter pelo menos um dígito (0-9).");
+Console.WriteLine("Deve conter pelo menos um caractere especial entre os seguintes:");
+Console.WriteLine("! @ # $ % ^ & * ( ) + = _ - { } [ ] : ; \\ \" ' ? < > , .");
+
+while (true)
+{
+    Console.WriteLine("\nDigite uma senha para validação:");
+
+    var str = Console.ReadLine() ?? string.Empty;
+    var regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_\-{}\[\]:;""'?<>,.]).{7,16}$");
+    
+    if (regex.IsMatch(str))
+    {
+        Console.WriteLine("Senha válida.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Senha inválida!");
+    }
+}
