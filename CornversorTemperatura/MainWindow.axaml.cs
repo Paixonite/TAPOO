c4344c32c52728fcdd3aef272f96c7e4b04eafd3
Vítor Paixão <paixaodamasceno@gmail.com>
2025-06-15 17:58:49 -0300

    renamed variables

--- CornversorTemperatura/MainWindow.axaml.cs
+++ CornversorTemperatura/MainWindow.axaml.cs
@@ -15,101 +15,101 @@ namespace MeuProjetoAvalonia
     	private void BtnClique_Click(object sender, RoutedEventArgs e)
     	{
 			// Recupera a referência ao TextBox
-			var txtBox = this.FindControl<TextBox>("txtInput");
-			var comboBox = this.FindControl<ComboBox>("conversionTypes");
-			var txtOutput = this.FindControl<TextBox>("txtOutput");
+			var inputTextBox = this.FindControl<TextBox>("txtInput");
+			var conversionComboBox = this.FindControl<ComboBox>("conversionTypes");
+			var outputTextBox = this.FindControl<TextBox>("txtOutput");
 
 			// Recupera e verifica o valor digitado no TextBox
-			if (!double.TryParse(txtBox.Text, out double valor))
+			if (!double.TryParse(inputTextBox.Text, out double value))
 			{
-				txtOutput.Text = "Valor inválido"; // Exibe erro se a conversão falhar
+				outputTextBox.Text = "Valor inválido"; // Exibe erro se a conversão falhar
 				return;
 			}
 
 			// Recupera a conversão selecionada
-			string conversaoSelecionada  = "";
-			if (comboBox.SelectedItem is ComboBoxItem selectedItem)
+			string selectedConversion = "";
+			if (conversionComboBox.SelectedItem is ComboBoxItem selectedItem)
 			{
-				conversaoSelecionada = selectedItem.Content.ToString();
+				selectedConversion = selectedItem.Content.ToString();
 			}
 
 			// Converte
-			double resultado = 0;
+			double result = 0;
 
-			switch (conversaoSelecionada)
+			switch (selectedConversion)
 			{
 				case "Celsius - Farenheit":
-					resultado = (valor * 1.8) + 32;
+					result = (value * 1.8) + 32;
 					break;
 
 				case "Farenheit - Celsius":
-					resultado = (valor - 32) / 1.8;
+					result = (value - 32) / 1.8;
 					break;
 
 				case "Celsius - Kelvin":
-					resultado = valor + 273.15;
+					result = value + 273.15;
 					break;
 
 				case "Kelvin - Celsius":
-					resultado = valor - 273.15;
+					result = value - 273.15;
 					break;
 
 				case "Metros - Pés":
-					resultado = valor * 3.28084;
+					result = value * 3.28084;
 					break;
 
 				case "Pés - Metros":
-					resultado = valor * 0.3048;
+					result = value * 0.3048;
 					break;
 
 				case "Quilômetros - Milhas":
-					resultado = valor * 0.621371;
+					result = value * 0.621371;
 					break;
 
 				case "Milhas - Quilômetros":
-					resultado = valor * 1.60934;
+					result = value * 1.60934;
 					break;
 
 				case "Gramas - Onças":
-					resultado = valor * 0.035274;
+					result = value * 0.035274;
 					break;
 
 				case "Onças - Gramas":
-					resultado = valor * 28.3495;
+					result = value * 28.3495;
 					break;
 
 				case "Quilogramas - Libras":
-					resultado = valor * 2.20462;
+					result = value * 2.20462;
 					break;
 
 				case "Libras - Quilogramas":
-					resultado = valor * 0.453592;
+					result = value * 0.453592;
 					break;
 
 				case "Litros - Galões":
-					resultado = valor * 0.264172;
+					result = value * 0.264172;
 					break;
 
 				case "Galões - Litros":
-					resultado = valor * 3.78541;
+					result = value * 3.78541;
 					break;
 
 				case "Mililitros - Onças Fluidas":
-					resultado = valor * 0.033814;
+					result = value * 0.033814;
 					break;
 
 				case "Onças Fluidas - Mililitros":
-					resultado = valor * 29.5735;
+					result = value * 29.5735;
 					break;
 
 				default:
-					resultado = 666;
+					result = 666;
 					break;
 			}
 
 			// Escreve valor na caixa output
-			txtOutput.Text = resultado.ToString();
-			//txtOutput.Text = conversaoSelecionada;
+			outputTextBox.Text = result.ToString();
+			//outputTextBox.Text = selectedConversion;
 
     	}
 	}
