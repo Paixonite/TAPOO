71632e54111ebb6008ba424dbb5266917160cf5d
Vítor Paixão <paixaodamasceno@gmail.com>
2025-06-18 16:52:36 -0300

    base code

--- FiltroBlur/Program.cs
+++ FiltroBlur/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Diagnostics;
+
+using FiltroBlur;
+
+var sw = Stopwatch.StartNew();
+var initialMemory = GC.GetTotalMemory(true);
+
+ImageProcessor.ProcessImages();
+
+var finalMemory = GC.GetTotalMemory(true);
+sw.Stop();
+
+Console.WriteLine($"Memória inicial: {initialMemory / 1024 / 1024:F2} MB");
+Console.WriteLine($"Memória final: {finalMemory / 1024 / 1024:F2} MB");
+Console.WriteLine($"Diferença de memória: {(finalMemory - initialMemory) / 1024 / 1024:F2} MB");
+Console.WriteLine($"Coleções GC Gen0: {GC.CollectionCount(0)}");
+Console.WriteLine($"Coleções GC Gen1: {GC.CollectionCount(1)}");
+Console.WriteLine($"Coleções GC Gen2: {GC.CollectionCount(2)}");
