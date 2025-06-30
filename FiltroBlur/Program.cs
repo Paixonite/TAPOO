74379007938816c3869838026214618ef01e2045
Vítor Paixão <153404890+Paixonite@users.noreply.github.com>
2025-06-30 00:12:07 -0300

    Implement Optimized version

--- FiltroBlur/Program.cs
+++ FiltroBlur/Program.cs
@@ -17,3 +17,18 @@ Console.WriteLine($"Diferença de memória: {(finalMemory - initialMemory) / 102
 Console.WriteLine($"Coleções GC Gen0: {GC.CollectionCount(0)}");
 Console.WriteLine($"Coleções GC Gen1: {GC.CollectionCount(1)}");
 Console.WriteLine($"Coleções GC Gen2: {GC.CollectionCount(2)}");
+
+// var sw = Stopwatch.StartNew();
+// var initialMemory = GC.GetTotalMemory(true);
+
+// OptimizedImageProcessor.ProcessImages();
+
+// var finalMemory = GC.GetTotalMemory(true);
+// sw.Stop();
+
+// Console.WriteLine($"Memória inicial: {initialMemory / 1024 / 1024:F2} MB");
+// Console.WriteLine($"Memória final: {finalMemory / 1024 / 1024:F2} MB");
+// Console.WriteLine($"Diferença de memória: {(finalMemory - initialMemory) / 1024 / 1024:F2} MB");
+// Console.WriteLine($"Coleções GC Gen0: {GC.CollectionCount(0)}");
+// Console.WriteLine($"Coleções GC Gen1: {GC.CollectionCount(1)}");
+// Console.WriteLine($"Coleções GC Gen2: {GC.CollectionCount(2)}");
